bf6abfdd9aeadd89b7bdd8dad04f0c58fc0623fa
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-07-08 15:49:16 -0300

    update

--- js/animacoes.js
+++ js/animacoes.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2500,
     dots: true,
     arrows: true
   });
